efa4677cb4bba5d924ef76f846aea2db239d3553
Ryan Pratt <pratt2999@gmail.com>
2016-05-02 17:28:03 -0400

    Added files via upload
    fixed functionality, added stylesheet

--- forecast.js
+++ forecast.js
@@ -2,81 +2,70 @@
 
 var app = angular.module('forecast', []);
 
-app.controller('testForecast1', function($http) {
-  
-  var vm = this;
-  
-  var URL = 'http://api.openweathermap.org/data/2.5/forecast/daily';
-  
-  var request = {
-    method: 'GET',
-    url: URL,
-    params: {
-       q: 'KansasCity',
-      mode: 'json',
-      units: 'imperial',
-      cnt: '7',
-      appid: 'a5aa2542e614ca58e5a204d215360781'
-    }
-  };
-  
-  $http(request)
-    .then(function(response) {
-      vm.data = response.data;
-    }).
-    catch(function(response) {
-      vm.data = response.data;
-    });
-});
+var key = 'a5aa2542e614ca58e5a204d215360781'; //API KEY
 
-app.controller('testForecast2', function($http) {
-  
-  var vm = this;
-  
-  var URL = 'http://api.openweathermap.org/data/2.5/weather?zip';
-  
-  var request = {
-    method: 'GET',
-    url: URL,
-    params: {
-    	 q: '44070', //ZIP CODE NOT WORKING AS EXPECTED, THIS MUST BE A CITY ID
-      mode: 'json',
-     appid: 'a5aa2542e614ca58e5a204d215360781'
-    }
-  };
-  
-  $http(request)
-    .then(function(response) {
-      vm.data = response.data;
-    }).
-    catch(function(response) {
-      vm.data = response.data;
-    });
-});
+app.controller('forecast', function($scope, $http, $log) {
+  console.log('call forecast');
 
-app.controller('testForecast3', function($http) {
+  $scope.displayForm = false;
+  $scope.location = ""; //DEFAULT IS WASHINGTON, DC
+
+  $scope.change = function() {
+    console.log('call change()');
+
+    $scope.displayForm = true;
+
+    var vm = this;
+
+    var URL = 'http://api.openweathermap.org/data/2.5/weather?zip';
   
-  var vm = this;
+    var request = {
+      method: 'GET',
+      url: URL,
+      params: {
+         q: $scope.location + ',us',
+        units: 'imperial',
+        mode: 'json',
+       appid: key
+      }
+    };
+
+    console.log('requesting daily weather data for: ' + $scope.location);
+      
+    $http(request)
+      .then(function(response) {
+        console.log('good');
+        vm.data_day = response.data;
+      }).
+      catch(function(response) {
+        console.log('bad');
+        vm.data_day = response.data;
+      });
   
-  var URL = 'http://api.openweathermap.org/data/2.5/forecast/daily';
-  
-  var request = {
-    method: 'GET',
-    url: URL,
-    params: {
-       q: '44070', //ZIP CODE NOT WORKING AS EXPECTED, THIS MUST BE A CITY ID
-      mode: 'json',
-      units: 'imperial',
-      cnt: '7',
-      appid: 'a5aa2542e614ca58e5a204d215360781'
-    }
-  };
-  
-  $http(request)
-    .then(function(response) {
-      vm.data = response.data;
-    }).
-    catch(function(response) {
-      vm.data = response.data;
-    });
+    var URL = 'http://api.openweathermap.org/data/2.5/forecast/daily';
+    
+    var request = {
+      method: 'GET',
+      url: URL,
+      params: {
+         q: $scope.location + ',us',
+        mode: 'json',
+        units: 'imperial',
+        cnt: '7',
+        appid: key
+      }
+    };
+
+    console.log('requesting weekly weather data for: ' + $scope.location);
+      
+    $http(request)
+      .then(function(response) {
+        console.log('good');
+        vm.data_week = response.data;
+      }).
+      catch(function(response) {
+        console.log('bad');
+        vm.data_week = response.data;
+      });
+    };
 });
